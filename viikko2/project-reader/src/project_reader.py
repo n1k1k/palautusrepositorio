9bc35061560f19e36a2a919e6c5a0f5c1b23e569
Nanna Ketola <nannailona@gmail.com>
2024-11-06 10:11:35 +0200

    Add toml parsing to project reader

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,23 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        #print(content)
+
+        parsed_toml = toml.loads(content)
+        #print(parsed_toml)
+
+        name = parsed_toml["tool"]["poetry"]["name"]
+        desc = parsed_toml["tool"]["poetry"]["description"]
+        dep = parsed_toml["tool"]["poetry"]["dependencies"]
+        dev_dep = parsed_toml["tool"]["poetry"]["group"]["dev"]["dependencies"]
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(name, desc, dep, dev_dep)
