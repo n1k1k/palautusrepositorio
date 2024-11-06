e6eeb9dc120fdf36caedbb391b64bb7c7693bd4f
Nanna Ketola <nannailona@gmail.com>
2024-11-06 10:18:57 +0200

    Expand projet reader

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -17,7 +17,10 @@ class ProjectReader:
 
         name = parsed_toml["tool"]["poetry"]["name"]
         desc = parsed_toml["tool"]["poetry"]["description"]
+        l = parsed_toml["tool"]["poetry"]["license"]
+        authors = parsed_toml["tool"]["poetry"]["authors"]
         dep = parsed_toml["tool"]["poetry"]["dependencies"]
         dev_dep = parsed_toml["tool"]["poetry"]["group"]["dev"]["dependencies"]
+        
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project(name, desc, dep, dev_dep)
+        return Project(name, desc, l, authors, dep, dev_dep)
