a09347a8283dcca4526944913917efae7f2861e4
Nanna Ketola <nannailona@gmail.com>
2024-11-24 12:49:50 +0200

    Add test for method lataa

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,19 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+    
+    def test_lataa_summa_kortille_jos_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 4
+        
+        self.kassa.lataa(maksukortti_mock, 10)
+
+        maksukortti_mock.lataa.assert_called()
+
+    def test_ei_lataa_summa_kortille_jos_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 4
+        
+        self.kassa.lataa(maksukortti_mock, -10)
+
+        maksukortti_mock.lataa.assert_not_called()
